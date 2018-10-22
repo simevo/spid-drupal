c89075e6d023572213cc9ffe32496ad37da57c54
Giulio Gatto <giuliogatto@gmail.com>
2018-10-22 16:27:44 +0200

    added main route

---
+++ src/Controller/SpidLoginController.php
@@ -0,0 +1,10 @@
+<?php
+namespace Drupal\spid_login\Controller;
+class SpidLoginController {
+    public function index() {
+        return array(
+                '#title' => 'SPID Login',
+                '#markup' => '<h2>Login Initial Route--</h2>',
+            );
+    }
+}

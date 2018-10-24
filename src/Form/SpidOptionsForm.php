e35ec217e5566d344a42f943f8cbe6e82e1f69e6
Giulio Gatto <giuliogatto@gmail.com>
2018-10-24 10:16:15 +0200

    added spid prefix to options

--- src/Form/SpidOptionsForm.php
+++ src/Form/SpidOptionsForm.php
@@ -26,25 +26,25 @@ class SpidOptionsForm extends ConfigFormBase {
   public function buildForm(array $form, FormStateInterface $form_state) {  
     $config = $this->config('spid_login.adminsettings');  
 
-    $form['provincia'] = [  
+    $form['spid_provincia'] = [  
       '#type' => 'textfield',  
       '#title' => $this->t('Provincia (nome per esteso)'),  
       '#description' => $this->t('Provincia del Service Provider'),  
-      '#default_value' => $config->get('provincia'),  
+      '#default_value' => $config->get('spid_provincia'),  
     ];  
 
-    $form['citta'] = [  
+    $form['spid_citta'] = [  
       '#type' => 'textfield',  
       '#title' => $this->t('Citt&aacute;'),  
       '#description' => $this->t('Citt&aacute;'),  
-      '#default_value' => $config->get('citta'),  
+      '#default_value' => $config->get('spid_citta'),  
     ];  
 
-    $form['email_referente'] = [  
+    $form['spid_email_referente'] = [  
       '#type' => 'textfield',  
-      '#title' => $this->t('Citt&aacute;'),  
+      '#title' => $this->t('Email referente'),  
       '#description' => $this->t("Indirizzo email dell'ente o del referente tecnico"),  
-      '#default_value' => $config->get('email_referente'),  
+      '#default_value' => $config->get('spid_email_referente'),  
     ];  
 
     return parent::buildForm($form, $form_state);  
@@ -54,9 +54,9 @@ class SpidOptionsForm extends ConfigFormBase {
     parent::submitForm($form, $form_state);  
 
     $this->config('spid_login.adminsettings')  
-      ->set('provincia', $form_state->getValue('provincia'))  
-      ->set('citta', $form_state->getValue('citta'))  
-      ->set('email_referente', $form_state->getValue('email_referente'))  
+      ->set('spid_provincia', $form_state->getValue('spid_provincia'))  
+      ->set('spid_citta', $form_state->getValue('spid_citta'))  
+      ->set('spid_email_referente', $form_state->getValue('spid_email_referente'))  
       ->save();  
   }  
 

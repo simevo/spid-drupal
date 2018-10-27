f44ff55bdd367627ffa84dbdd031511c7fc307d7
Giulio Gatto <giuliogatto@gmail.com>
2018-10-27 11:59:51 +0200

    added logout and clean code

--- src/Controller/SpidLoginController.php
+++ src/Controller/SpidLoginController.php
@@ -1,33 +1,9 @@
 <?php
 namespace Drupal\spid_login\Controller;
-define( 'SPID_DRUPAL_PATH', dirname(__FILE__, 3) . '/' ); // REMOVE THIS CONSTANT!!!
 
 use Drupal\Core\Controller\ControllerBase;
 use Symfony\Component\DependencyInjection\ContainerInterface;
 use Drupal\spid_login\Services\SpidService;
-
-// NOTE: using hardcoded class from italia/spid-php-lib because of hotfix
-require_once SPID_DRUPAL_PATH . 'spid-php-lib/src/Spid/Interfaces/IdpInterface.php';
-require_once SPID_DRUPAL_PATH . 'spid-php-lib/src/Spid/Interfaces/RequestInterface.php';
-require_once SPID_DRUPAL_PATH . 'spid-php-lib/src/Spid/Interfaces/ResponseInterface.php';
-require_once SPID_DRUPAL_PATH . 'spid-php-lib/src/Spid/Interfaces/SAMLInterface.php';
-require_once SPID_DRUPAL_PATH . 'spid-php-lib/src/Spid/Saml/Idp.php';
-require_once SPID_DRUPAL_PATH . 'spid-php-lib/src/Spid/Saml/Settings.php';
-require_once SPID_DRUPAL_PATH . 'spid-php-lib/src/Spid/Saml/SignatureUtils.php';
-require_once SPID_DRUPAL_PATH . 'spid-php-lib/src/Spid/Saml/In/BaseResponse.php';
-require_once SPID_DRUPAL_PATH . 'spid-php-lib/src/Spid/Saml/In/LogoutRequest.php';
-require_once SPID_DRUPAL_PATH . 'spid-php-lib/src/Spid/Saml/In/LogoutResponse.php';
-require_once SPID_DRUPAL_PATH . 'spid-php-lib/src/Spid/Saml/In/Response.php';
-require_once SPID_DRUPAL_PATH . 'spid-php-lib/src/Spid/Saml/Out/Base.php';
-require_once SPID_DRUPAL_PATH . 'spid-php-lib/src/Spid/Saml/Out/AuthnRequest.php';
-require_once SPID_DRUPAL_PATH . 'spid-php-lib/src/Spid/Saml/Out/LogoutRequest.php';
-require_once SPID_DRUPAL_PATH . 'spid-php-lib/src/Spid/Saml/Out/LogoutResponse.php';
-require_once SPID_DRUPAL_PATH . 'spid-php-lib/src/Spid/Saml.php';
-require_once SPID_DRUPAL_PATH . 'spid-php-lib/src/Spid/Session.php';
-require_once SPID_DRUPAL_PATH . 'spid-php-lib/src/Sp.php';
-// remove previous require_once when class spid-php-lib is fixed 
-
-require_once SPID_DRUPAL_PATH . 'vendor/autoload.php';
 
 class SpidLoginController extends ControllerBase{
 
@@ -78,6 +54,8 @@ class SpidLoginController extends ControllerBase{
     }
 
     public function logout() {
+
+        $this->spid_service->auth->logout(0); // investigate what is the required $slo parameter to pass to logout()
 
         return array(
                 '#title' => 'SPID Login',
